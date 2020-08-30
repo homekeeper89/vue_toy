3583244b3d0c074aacc790bc44fec435f7d24e0e
matthew <matthew@cupist.com>
2020-08-30 23:47:59 +0900

    [modify] factory 로 리팩토링

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -1,14 +1,17 @@
 import { shallowMount } from '@vue/test-utils';
 import SubmitButton from '@/components/SubmitButton.vue';
 
+const factory = (propsData) => {
+  return shallowMount(SubmitButton, {
+    propsData: {
+      ...propsData,
+    },
+  });
+};
+
 describe('SubmitButton.vue', () => {
   it('인증 되지 않은 메세지를 표시한다', () => {
-    const msg = 'submit';
-    const wrapper = shallowMount(SubmitButton, {
-      propsData: {
-        msg: msg,
-      },
-    });
+    const wrapper = factory({ msg: 'submit' });
 
     console.log(wrapper.html());
 
@@ -19,12 +22,7 @@ describe('SubmitButton.vue', () => {
   it('관리자 권한 메세지를 표시한다', () => {
     const msg = 'submit';
     const isAdmin = true;
-    const wrapper = shallowMount(SubmitButton, {
-      propsData: {
-        msg: msg,
-        isAdmin: isAdmin,
-      },
-    });
+    const wrapper = factory({ isAdmin: true, msg: 'submit' });
 
     expect(wrapper.find('span').text()).toBe('Admin Privileges');
     expect(wrapper.find('button').text()).toBe('submit');

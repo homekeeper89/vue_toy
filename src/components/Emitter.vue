2a7147345f2e9c70c9a20761c5bcf41b45d1715f
matthew <matthew@cupist.com>
2020-09-02 22:16:18 +0900

    [add] emit test 추가
    emit의 내용을 확인할 수 있다
    
    wrapper.emitted() 가 객체를 가지고 있음

---
+++ src/components/Emitter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'Emitter',
+  methods: {
+    emitEvent() {
+      this.$emit('myEvent', 'name', 'password');
+    },
+  },
+};
+</script>

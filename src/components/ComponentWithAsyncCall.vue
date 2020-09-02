80ab3b94addf5a89247e316f67d9145c316a13cd
matthew <matthew@cupist.com>
2020-09-03 00:14:11 +0900

    [add] stub을 사용하는 방법
    mount + stubs
    또는 shallowMount

---
+++ src/components/ComponentWithAsyncCall.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'ComponentWithAsyncCall',
+  created() {
+    this.makeApiCall();
+  },
+  methods: {
+    async makeApiCall() {
+      console.log('Making api call');
+      await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    },
+  },
+};
+</script>

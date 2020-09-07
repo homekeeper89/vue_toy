163638e35e60f6969b24fea25834ecd088251549
matthew <matthew@cupist.com>
2020-09-07 21:47:33 +0900

    [add] route params 테스트 추가

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -7,10 +7,11 @@ import routes from '@/router/routes.js';
 const localVue = createLocalVue();
 localVue.use(VueRouter);
 
-jest.mock('@/components/NestedRoute.vue', () => ({
-  name: 'NestedRoute',
-  render: (h) => h('div'),
-}));
+// 이거 키면 두번째 테스트 작동 안함
+// jest.mock('@/components/NestedRoute.vue', () => ({
+//   name: 'NestedRoute',
+//   render: (h) => h('div'),
+// }));
 
 describe('App', () => {
   it('라우팅을 통해서 자식 컴포넌트 렌더', async () => {
@@ -25,5 +26,18 @@ describe('App', () => {
     await wrapper.vm.$nextTick();
 
     expect(wrapper.findComponent(NestedRoute).exists()).toBe(true);
+  });
+
+  it('쿼리 틀트링으로부터 username을 렌더', () => {
+    const username = 'alice';
+    const wrapper = shallowMount(NestedRoute, {
+      mocks: {
+        $route: {
+          params: { username },
+        },
+      },
+    });
+
+    expect(wrapper.find('.username').text()).toBe(username);
   });
 });
